865d83a8833ee7b0366287b5932333385aea2afe
Richard15 <richardantonioassistrujillo@gmail.com>
2024-11-22 16:30:40 -0500

    update name

--- src/main/java/com/API/Sistema/de/Inventario/StockGeniusApplication.java
+++ src/main/java/com/API/Sistema/de/Inventario/StockGeniusApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaDeInventarioApplication {
+public class StockGeniusApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemaDeInventarioApplication.class, args);
+		SpringApplication.run(StockGeniusApplication.class, args);
 	}
 
 }
